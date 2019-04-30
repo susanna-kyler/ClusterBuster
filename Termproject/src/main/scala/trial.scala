6a3f10832bd393845e906c9db018ac1f3df7a012
Nate Mortensen <n@temortensen.com>
2019-04-30 14:34:36 -0600

    Finish stock dataframe

--- Termproject/src/main/scala/trial.scala
+++ Termproject/src/main/scala/trial.scala
@@ -9,50 +9,52 @@ object trial {
   val ROWS_AHEAD = 10
   val ROWS_BEHIND = 3
   def main(args: Array[String]) {
-    val inputFile = args(0)
-    val outputFile = args(1)
+    val tweetFile = args(0)
+    val stockFile = args(1)
+    val outputFile = args(2)
 
     val spark = SparkSession.builder().appName("TrialByFire").getOrCreate()
 
-    val dataFrame = spark.read.json(inputFile)
+    val stocks = getStocks(spark, stockFile)
+    val dataFrame = spark.read.json(tweetFile)
     //if using shell instead of the above line use :
     //    val dataFrame = spark.read.json("hdfs:///twitter/2016/01/01/00/30.json.bz2")
 
     val columns = dataFrame.select("created_at", "text")
 
-    val nnNulls = columns.filter(_(0)!= null)
-    val nonNulls = nnNulls.filter(_(1)!= null)
-    val filtTweets = nonNulls.filter(_(1).asInstanceOf[String].contains("Apple"))
-
-    // Save the word count back out to a text file, causing evaluation.
-    if(filtTweets.count() > 0){    filtTweets.write.csv(outputFile) }
+    if (stocks.count() > 0) {
+      stocks.write.csv(outputFile)
+    }
   }
 
   /**
     * Constructs a new DataFrame containing rows which have the following:
-    * Stock, Date, Open, High, Low, Close, Volume, OpenInt, diff_1...diff_10, prev_avg
+    * Stock, Date, Open, High, Low, Close, Volume, OpenInt, diff_0...diff_10, prev_avg
     * @param spark SparkSession to use
     * @param stockSource Path to the stock data files
     * @return
     */
-  def getPrices(spark: SparkSession, stockSource: String) : DataFrame = {
+  def getStocks(spark: SparkSession, stockSource: String) : DataFrame = {
     import spark.implicits._
-    val csv = spark.read.csv(stockSource)
+    val csv = spark.read.format("csv").option("header", true).load(stockSource)
     // Convert file name to stock name
-    val mapName = (fileName: String) => fileName.substring(0, fileName.indexOf('.'))
-    // Construct a Spark User Defined Function
-    val nameMapper = udf(mapName)
+    //
+    val mapName = udf((fileName: String) => fileName.substring(fileName.lastIndexOf('/')+1, fileName.indexOf('.')))
     // Add a new column with the stock name
-    var withWindows = csv.withColumn("Stock", nameMapper(input_file_name()))
+    var withWindows = csv.withColumn("Stock", mapName(input_file_name()))
 
     val window = Window.partitionBy($"Stock").orderBy($"Date")
-    for (i <- 1 to ROWS_AHEAD) {
-      val relative = lead(csv("Close"), i).over(window)
-      // I've got no idea how the $ thing works.
-      withWindows = withWindows.withColumn("diff_"+i, $"Close" - relative)
-    }
     val prevWindow = window.rowsBetween(Window.currentRow - ROWS_BEHIND, Window.currentRow)
     withWindows = withWindows.withColumn("prev_avg", avg($"Close").over(prevWindow))
+    for (i <- 0 to ROWS_AHEAD) {
+      val relative = lead(csv("Close"), i).over(window)
+      val computeDiff = udf((current: Double, future: Double) => {
+        Option[Double](future).map(_ - current).getOrElse(0D)
+      })
+      // Difference between previous average and the i-th away day
+      withWindows = withWindows.withColumn("diff_"+i, computeDiff($"prev_avg", relative))
+    }
+
 
     withWindows
   }
