f189ed619845a00fc2fc59c98dc6047d7ad704a5
Nate Mortensen <n@temortensen.com>
2019-05-01 12:50:32 -0600

    Cleanup and completion
    In theory this is pretty much done but I haven't been able
    to fully test it yet.  I left some comments above
    the methods to describe using them in the shell.

--- Termproject/src/main/scala/trial.scala
+++ Termproject/src/main/scala/trial.scala
@@ -4,12 +4,13 @@ import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.Matrix
-import org.apache.spark.ml.stat.{ChiSquareTest, Correlation}
-import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.apache.spark.ml.stat.Correlation
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object trial {
   val ROWS_AHEAD = 10
   val ROWS_BEHIND = 3
+  val DIFF_ROWS = (0 to ROWS_AHEAD).map("diff_"+_).toArray
 
   def main(args: Array[String]) {
     val tweetFile = args(0)
@@ -17,15 +18,56 @@ object trial {
     val outputFile = args(2)
 
     val spark = SparkSession.builder().appName("TrialByFire").getOrCreate()
-    //if using shell instead of the below line, use :   val tweetData = spark.read.json("hdfs:///twitter/2016/01/01/00")
-    var tweetData = spark.read.json(tweetFile)
+
+    val stocks = getStocks(spark, stockFile)
+    val tweets = getTweetCounts(spark, tweetFile)
+    // Join the stocks with the tweet counts.  Gives us Stock, created_at, Date, change_in_tweets, tweet_count, and all the stock fields
+    val joinStocksTweets = stocks.join(tweets, Seq("Date", "Stock"))
+
+    // Write it out for our own sanity
+    joinStocksTweets.write.csv(outputFile)
+
+    // Test for correlations between the change in tweets and diff_0...diff_ROWS_AHEAD
+    val stats = testColumns(joinStocksTweets, "change_in_tweets", DIFF_ROWS:_*)
+    for ((key, value) <- stats) {
+      println(s"Column: $key has a correlation coeff of ${value.correlation}, a p-value of ${value.pValue}, and ${value.dof} degrees of freedom")
+    }
+
+    // Idk if this is needed, I've seen it in a few examples online
+    spark.close()
+  }
+
+  // The methods below are useful for interacting with these dataframes in the spark shell.
+  // To load this file, use:
+  // :load path/to/file
+  // All methods have default values for the files so you can just call them via:
+  // trial.getTweetCounts(spark) or trial.getStocks(spark)
+  // Spark is surprisingly generous with wildcards, so you can do something like:
+  //   val tweets = trial.getTweetCounts(spark, "hdfs:///twitter/2016/01/*/00")
+  // in order to get a minor sampling of many days
+
+  /**
+    * Constructs a Dataframe of aggregate assocations between tweets and companies.
+    * Has the following fields:
+    * Stock, Date, tweet_count, change_in_tweets
+    * @param spark
+    * @param tweetSource
+    * @param companySource
+    * @return
+    */
+  def getTweetCounts(spark: SparkSession, tweetSource: String = "hdfs:///twitter/2016/01/01/00", companySource : String = "hdfs:///companys/comps2.csv") : DataFrame = {
+    var tweetData = spark.read
+        .json(tweetSource)
     // getting company names
-    val nameData  = spark.read.csv("hdfs:///companys/comps2.csv")
+    val nameData  = spark.read.csv(companySource)
+      .withColumnRenamed("_c0", "Stock")
+      .withColumnRenamed("_c1", "Name")
+      .withColumnRenamed("_c2", "Nicknames")
     // Only getting two components of a tweet
     tweetData = tweetData.select("created_at", "text")
     tweetData  = tweetData.filter(_(0)!= null).filter(_(1) !=null)
 
-    val contained = udf{(tweet:String, name:String, nickName: String) => {
+    val mentionsCompany = udf{(tweet:String, name:String, nickName: String) => {
       val tweetWords = tweet.split(" |\\.|#")
       if(nickName!= null) {
         val nicks = nickName.split(", ")
@@ -34,9 +76,10 @@ object trial {
       else  tweetWords.exists(_.contains(name))
     }}
 
-    var companyTweets = tweetData.join( nameData, contained(tweetData("text"), nameData("_c1"),nameData("_c2")))
+    // Join the tweets with the companies that they mention using our custom matching function
+    var companyTweets = tweetData.join( nameData, mentionsCompany(tweetData("text"), nameData("Name"),nameData("Nicknames")))
 
-    // Function transforms the "created_at" column to = month day
+    // Function transforms the "created_at" column to = YYYY-MM-dd
     val applyTime = udf{(timeUnit: String) => {
       val m = timeUnit.substring(4,7)
       val day = timeUnit.substring(9,11)
@@ -57,26 +100,24 @@ object trial {
       "2016-"+month+"-"+day
     }}
 
-    companyTweets= companyTweets.withColumn("created_at", applyTime(tweetData("created_at"))) // created_at, text, _c0, _c1, _c2
-//    companyTweets.write.csv(outputFile)
+    // Standardize the date format between datasets
+    companyTweets= companyTweets.withColumn("created_at", applyTime(tweetData("created_at"))) // created_at, text, Stock, Name, Nicknames
+    //    companyTweets.write.csv(outputFile)
 
-    val stocks = getStocks(spark, stockFile)
+    // Compute number of tweets about each company each day
+    var grouped = companyTweets.groupBy("Stock","created_at" ).agg(count("*").as("tweet_count"))
 
-    //group by stock symbol and date
-    val grouped = companyTweets.groupBy("_c0","created_at" ).agg(count("*")).as("cnt")
+    val window = Window.partitionBy(grouped("Stock")).orderBy(grouped("created_at"))
+    val previousDay = lag(grouped("tweet_count"), 1).over(window)
 
-    val joinStocksTweets = stocks.join(grouped, grouped("created_at") <=> stocks("Date") && grouped("_c0") <=> stocks("Stock"), "left")
+    // Compute percentage change from the previous day for each company.  If they had zero tweets on either day this will be NaN
+    grouped = grouped.withColumn("change_in_tweets", grouped("tweet_count") - previousDay)
+    grouped = grouped.filter(grouped("change_in_tweets").isNotNull)
+    // Rename the column to match the stock one.  Makes the join easier
+    grouped = grouped.withColumnRenamed("created_at", "Date")
 
-    joinStocksTweets.write.csv(outputFile)
-
+    grouped
   }
-  
-
-  // Truncate dates
-  // x =map date,ID combo to count
-  // join x with stock data on ID and Date
-  // perform stats
-
   /**
     * Constructs a new DataFrame containing rows which have the following:
     * Stock, Date, Open, High, Low, Close, Volume, OpenInt, diff_0...diff_10, prev_avg
@@ -84,9 +125,12 @@ object trial {
     * @param stockSource Path to the stock data files
     * @return
     */
-  def getStocks(spark: SparkSession, stockSource: String) : DataFrame = {
+  def getStocks(spark: SparkSession, stockSource: String = "hdfs:///stocks/Stocks") : DataFrame = {
     import spark.implicits._
-    var csv = spark.read.format("csv").option("header", true).load(stockSource)
+    var csv = spark.read
+        .format("csv")
+        .option("header", true)
+        .load(stockSource)
 
     //filters out non 2016 years
     val filterFor2016 = udf{(year: String) => { year.substring(0,4).equals("2016") }}
@@ -96,35 +140,45 @@ object trial {
     // Convert file name to stock name
     val mapName = udf((fileName: String) => fileName.substring(fileName.lastIndexOf('/')+1, fileName.indexOf('.')).toUpperCase)
     // Add a new column with the stock name
-    var withWindows = csv.withColumn("Stock", mapName(input_file_name()))
+    csv = csv.withColumn("Stock", mapName(input_file_name()))
 
     val window = Window.partitionBy($"Stock").orderBy($"Date")
-    val prevWindow = window.rowsBetween(Window.currentRow - ROWS_BEHIND, Window.currentRow)
-    withWindows = withWindows.withColumn("prev_avg", avg($"Close").over(prevWindow))
+    val prevWindow = window.rowsBetween(Window.currentRow - ROWS_BEHIND, Window.currentRow - 1)
+    // Get the average of the past three days, and remove any rows that don't have three previous days(AKA the first day)
+    csv = csv.withColumn("prev_avg", avg($"Close").over(prevWindow))
+    csv = csv.filter($"prev_avg".isNotNull)
+    // Add a new column for each day up to ROWS_AHEAD
     for (i <- 0 to ROWS_AHEAD) {
       val relative = lead(csv("Close"), i).over(window)
-      val computeDiff = udf((current: Double, future: Double) => {
-        Option[Double](future).map(_ - current).getOrElse(0D)
-      })
-      // Difference between previous average and the i-th away day
-      withWindows = withWindows.withColumn("diff_"+i, computeDiff($"prev_avg", relative))
+
+      // Difference between previous average and the i-th away day, and remove any rows that don't have that many rows ahead.  This removes the last ROWS_AHEAD rows.
+      csv = csv.withColumn("diff_"+i, relative - $"prev_avg")
+      csv = csv.filter(csv("diff_"+i).isNotNull)
     }
 
-    withWindows
+    csv
   }
 
+  /**
+    * Contains the statistics we collect for a single correlation
+    * @param pValue the p-value determining the statistic significance of the correlation
+    * @param dof the degrees of freedom, which is just the number of rows.
+    * @param correlation -1 to 1 indicating the strength of the correlation.  This is measuring a monotonic correlation(not a linear one),
+    *                    so it's not very straightforward to interpret.
+    */
   case class Stats(pValue: Double, dof: Double, correlation: Double)
 
   /**
     * Performs a spearman correlation test to see if the independent variable is monotonically correlated with the specified dependent variables.
     *
-    * @param data
+    * @param data dataframe containing the columns
     * @param independent the column to use as the independent variable
     * @param dependent sequence of potentially dependent columns
     * @return a mapping of dependent variable => stats about the correlation
     */
   def testColumns(data: DataFrame, independent: String, dependent: String*) : Map[String, Stats] = {
     val featureArray = (dependent ++ Seq(independent)).toArray[String]
+    // Add all the specified columns into a single vector, which is how Spark likes to do its stats.
     val assembler = new VectorAssembler()
         .setInputCols(featureArray)
         .setOutputCol("features")
